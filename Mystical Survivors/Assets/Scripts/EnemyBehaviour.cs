3daf3e454e928d3e21a9806458804c3050d43b40
TerrenceGunawan <135194407+TerrenceGunawan@users.noreply.github.com>
2024-07-27 19:48:19 +1000

    Player health and enemies deal damage

--- Mystical Survivors/Assets/Scripts/EnemyBehaviour.cs
+++ Mystical Survivors/Assets/Scripts/EnemyBehaviour.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 public class EnemyBehavior : MonoBehaviour
 {
     public float moveSpeed = 3f;
+    public float cooldown = 100f;
     private Transform player;
     private Rigidbody2D rb;
+    private PlayerStats P;
+
+
 
     void Start()
     {
         player = GameObject.FindWithTag("Player").transform;
         rb = GetComponent<Rigidbody2D>();
+
+        GameObject playerObject = GameObject.FindWithTag("Player");
+        if (playerObject != null)
+        {
+            P = playerObject.GetComponent<PlayerStats>();
+        }
+
     }
 
     void Update()
@@ -19,13 +30,34 @@ public class EnemyBehavior : MonoBehaviour
             Vector2 direction = (player.position - transform.position).normalized;
             rb.velocity = direction * moveSpeed;
         }
+        if(cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collider.gameObject.CompareTag("Player"))
         {
-            // Handle collision with player (e.g., reduce player health)
+            if(cooldown < 0)
+            {
+            P.playerHealth-=10;
+            Debug.Log("ow");
+            cooldown = 1;
+            }
+        }
+    }
+      void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            if(cooldown < 0)
+            {
+            P.playerHealth-=10;
+            Debug.Log("owie");
+            cooldown = 1;
+            }
         }
     }
 }
---
+++ Mystical Survivors/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+public float playerHealth = 100;
+public float playerXP;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
